52a0aa4085f02ae068b3cf5739e46d06993172ef
EileenYXu <72393146+EileenYXu@users.noreply.github.com>
2025-05-10 10:53:29 +0100

    Reorganised scripts, updated PRS analyses to use larger PRS sample

--- SCRIPTS/PREP/prep_y2dat.R
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-dat = readRDS("G://users/eileen/ABCD/ABCD_Environmental_Risk/ABCDv5.1/DATA/predictors_unrelatedIDs.rds")
-y2 = dat %>% filter(eventname=="2_year_follow_up_y_arm_1") %>% select_if(~ !all(is.na(.)))
-
-names(y2)
-sapply(y2, function(x)sum(is.na(x))) |> sort(decreasing = T)
-       
